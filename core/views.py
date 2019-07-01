25f1ab2999372476ae91cf023a69cd53d33c5480
Herculano <herculano7@gmail.com>
2019-07-01 10:55:03 -0300

    alterei view

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 def Estilos(request)
 	return render(request):
@@ -6,3 +8,24 @@ def Estilos(request)
 
 def Dancarino(request)
 	return render(request):
+
+
+def inscritos(request)
+	return render(request, 'inscritos.html')
+
+
+# Create your views here.
+def inicial(request):
+	return render(request, 'index.html')
+@login_required
+def perfil(request):
+	return render(request, 'perfil.html')
+def registro(request):
+	form = UserCreationForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		return redirect('login')
+	contexto = {
+	'form': form
+	}
+	return render(request, 'registro.html', contexto)
